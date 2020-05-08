3f42cab5e76841671b8fa3bc44fcf49613b2d446
Geoffrey <geoolson@pdx.edu>
2020-05-07 19:47:53 -0700

    finished exercise 2

--- assign3/2-reverse.js
+++ assign3/2-reverse.js
@@ -1,1 +1,10 @@
 // Enter your code here
+const form = document.getElementsByTagName('form')[0];
+const input = document.getElementById('input'); 
+form.onsubmit = e => {
+    e.preventDefault();
+    if(input.value.length === 8 || isNaN(input.value))
+        alert([...input.value].reverse().join(''));
+    else
+        alert('Please enter an 8 digit number');
+}
