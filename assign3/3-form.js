c3c71768302c467ea8b4e82802ad2810c3a1b655
Geoffrey <geoolson@pdx.edu>
2020-05-07 23:01:44 -0700

    exercise three

--- assign3/3-form.js
+++ assign3/3-form.js
@@ -1,1 +1,9 @@
 // Enter your code here
+const [form] = document.getElementsByTagName('form');
+form.onsubmit = e => {
+    e.preventDefault();
+    console.log(`name: ${document.getElementById('name').value}`);
+    console.log(`email: ${document.getElementById('email').value}`);
+    console.log(`comments: ${document.getElementById('comments').value}`);
+    console.log(`newsletter: ${document.getElementById('newsletter').value}`);
+}
