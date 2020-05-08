0c2cfd747bc4db35c2b0e6dd5e7ba1088547b048
Geoffrey <geoolson@pdx.edu>
2020-05-07 20:06:19 -0700

    updated conditional

--- assign3/2-reverse.js
+++ assign3/2-reverse.js
@@ -3,7 +3,7 @@ const form = document.getElementsByTagName('form')[0];
 const input = document.getElementById('input'); 
 form.onsubmit = e => {
     e.preventDefault();
-    if(input.value.length === 8 || isNaN(input.value))
+    if(input.value.length === 8 && !isNaN(input.value))
         alert([...input.value].reverse().join(''));
     else
         alert('Please enter an 8 digit number');
