307de83d07474c841f4fd32a9ed516f7fed5696f
Geoffrey <geoolson@pdx.edu>
2020-05-07 19:10:39 -0700

    finished exercise 1

--- assign3/1-fizzbuzz.js
+++ assign3/1-fizzbuzz.js
@@ -1,1 +1,13 @@
 // Enter your code here
+const fizzBuzz = () => {
+    let result = ''
+    for(let i = 1; i <= 100; ++i){
+        let line = i%3 === 0 ?'fizz':'';
+        line += i%5 === 0 ?'buzz':'';
+        line = line === "" ? i : line;
+        result += `<p>${line}</p>`;
+    }
+    return result;
+};
+const results = document.getElementById("results");
+results.innerHTML = fizzBuzz();
