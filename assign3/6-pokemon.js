e8e96473a618e0b41348592a8f49d6fce3f99c23
Geoffrey <geoolson@pdx.edu>
2020-05-07 20:40:05 -0700

    using async and await now

--- assign3/6-pokemon.js
+++ assign3/6-pokemon.js
@@ -1,12 +1,13 @@
 const url = 'https://pokeapi.co/api/v2/pokemon/';
 
 // Enter your code here
-fetch(`${url}?limit=${10000}`)
-    .then(res => res.json())
-    .then(data => {
-        const resultDiv = document.getElementById('results');
-        resultDiv.innerHTML = data.results.reduce( (acc, curr) => {
-            const name = curr.name.charAt(0).toUpperCase() + curr.name.slice(1);
-            return acc + `<span class="d-block">${name}</span>`;
-        },'');
-    });
+async function getAllPokemon() {
+    let res = await fetch(`${url}?limit=${10000}`);
+    let data = await res.json();
+    const resultDiv = document.getElementById('results');
+    resultDiv.innerHTML = data.results.reduce( (acc, curr) => {
+        const name = curr.name.charAt(0).toUpperCase() + curr.name.slice(1);
+        return acc + `<span class="d-block">${name}</span>`;
+    },'');
+}
+getAllPokemon();
