8b7590b6e36b07ca65723f3d1b0ec29ab7798074
Geoffrey <geoolson@pdx.edu>
2020-05-07 21:49:36 -0700

    finsihed exercise four

--- assign3/4-events.js
+++ assign3/4-events.js
@@ -1,1 +1,6 @@
 // Enter your code here
+const [ divWidth, divHeight ] = document.getElementsByTagName('div');
+window.onresize = () => {
+    divWidth.innerText = window.innerWidth;
+    divHeight.innerText = window.innerHeight;
+}
