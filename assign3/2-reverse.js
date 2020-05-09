5e8045ac591f826919ee7d2adc41a207683c0c0a
Geoffrey <geoolson@pdx.edu>
2020-05-08 21:01:39 -0700

    handles negative numbers now

--- assign3/2-reverse.js
+++ assign3/2-reverse.js
@@ -1,9 +1,15 @@
 // Enter your code here
-const form = document.getElementsByTagName('form')[0];
-const input = document.getElementById('input'); 
+const [form] = document.getElementsByTagName('form');
+const input = document.getElementById('input');
 form.onsubmit = e => {
     e.preventDefault();
-    if(input.value.length === 8 && !isNaN(input.value))
+    if (isNaN(input.value)) {
+        alert('Please enter an 8 digit number');
+        return;
+    }
+    else if (input.value < 0 && input.value.length === 9)
+        alert('-' + [...input.value].slice(1).reverse().join(''));
+    else if (input.value.length === 8 && input.value >= 0)
         alert([...input.value].reverse().join(''));
     else
         alert('Please enter an 8 digit number');
