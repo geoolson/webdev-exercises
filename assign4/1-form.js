704322a994e2e9a1691e398efb20d5f534a1d50b
Geoffrey <geoolson@pdx.edu>
2020-05-15 21:34:45 -0700

    finished exercise one

--- assign4/1-form.js
+++ assign4/1-form.js
@@ -1,1 +1,29 @@
 // Enter your code here
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = 8080;
+const indexPage = fs.readFileSync('index.html', 'utf8');
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) =>{
+    res.send(indexPage);
+})
+
+app.post('/submit', (req, res) => {
+    const { name, email, comments } = req.body;
+    res.send(`
+    <html lang="en">
+        <head>
+            <title>Form Response</title>
+        </head>
+        <body>
+            name: ${name} <br>
+            email: ${email} <br>
+            comments: ${comments}
+        </body>
+    </html>`);
+});
+
+app.listen(port, () => console.log(`listening on port: ${port}`));
